0fd49230733ca039b2f576c1561443e08b1e2e03
lauramunozvensure <laura.munoz@vensure.com>
2024-12-11 16:48:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {UseState} from './UseState.js';
+import {ClassState} from './ClassState.js';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UseState/>
+      <ClassState/>
+    </div>
+  );
+}
+
+export default App;
